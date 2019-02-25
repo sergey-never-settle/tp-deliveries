bf85ca7a292a3cb9f7a8fd9dd233d1953f829c02
Sergey Shpota <od311289ssv@gmail.com>
2019-02-25 19:58:08 +0200

    Updates

---
+++ precache-manifest.c1afb65a0c350d81c5265a0c1038779d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tp-deliveries/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "aeb763379660c870bca2",
+    "url": "/tp-deliveries/static/js/runtime~main.aeb76337.js"
+  },
+  {
+    "revision": "bc322e9506d1ce65f4d9",
+    "url": "/tp-deliveries/static/js/main.bc322e95.chunk.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "/tp-deliveries/static/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "bc322e9506d1ce65f4d9",
+    "url": "/tp-deliveries/static/css/main.e948dd40.chunk.css"
+  },
+  {
+    "revision": "8afcf0ed78e4570aec877f63d590b55e",
+    "url": "/tp-deliveries/index.html"
+  }
+];
